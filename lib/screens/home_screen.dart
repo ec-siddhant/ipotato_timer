bb7074a4aea2830b5cde107654b5a35a23851915
ec-siddhant <siddhant@elementalconcept.com>
2022-11-24 23:12:58 +0530

    app bar setup

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:ipotato_timer/widgets/custom_app_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-        child: Scaffold());
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 120,
+        flexibleSpace: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 12.0, left: 31),
+                child: Text(
+                  'Potato Timer',
+                  style: Theme.of(context).textTheme.headlineLarge,
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              )
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+      ),
+      body: ListView(
+        children: const [],
+      ),
+    ));
   }
 }
